0d1e11033b0825d74624c97545b345c82e19db8b
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-05-18 21:13:16 +0200

    Añadido boton para cerrar sesion desde inventario

---
+++ static/js/inventario.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutButton = document.getElementById("logout-button");
+
+    logoutButton.addEventListener("click", function () {
+        // Aquí puedes agregar la lógica para cerrar sesión cuando tengas la autenticación de usuarios implementada
+
+        // Redirige a la página de inicio después de cerrar sesión
+        window.location.href = "/home";
+    });
+});

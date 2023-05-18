f2762f2769e54c1803f0f03e9251cdf8bcc1a856
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-05-18 20:53:39 +0200

    formulario de inicio de sesión

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,15 @@
-console.log("Hello from main.js");
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        console.log("Usuario:", username);
+        console.log("Contraseña:", password);
+
+        // Aquí puedes agregar la lógica para iniciar sesión cuando tengas la base de datos y el registro de usuarios implementado
+    });
+});

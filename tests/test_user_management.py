83a2ba34e22d4f270591ff53ad5e0f2ada7fee45
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-08-05 16:16:26 +0200

    No tests

--- tests/test_user_management.py
+++
@@ -1,9 +0,0 @@
-# Pruebas relacionadas con la gestión de usuarios.
-from unittest.mock import patch
-from src.connection import get_user_by_id
-
-def test_get_user_by_id():
-    with patch("src.connection.get_user_by_id") as mock_get_user:
-        mock_get_user.return_value = None
-        user = get_user_by_id(0)
-        assert user is None

18a7daab7673605a1d9a3b451212f0b474f323da
Alejandro Vega Vaquero <agevega@hotmail.com>
2023-08-09 11:24:06 +0200

    Creada funcion execute_query

--- src/admin.py
+++ src/admin.py
@@ -1,10 +1,7 @@
-# admin.py
-from .connection import get_connection
-from psycopg2 import extras
+from .connection import execute_query
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-# Definición de la clase usuario
 class Admin(UserMixin):
     def __init__(self, id, nickname, password):
         self.id = id
@@ -15,90 +12,53 @@ class Admin(UserMixin):
         return check_password_hash(self.password, password)
 
 
-# Funcion para conseguir un cursor
-def get_cursor():
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    return conn, cur
-
-# Funcion para cerrar conexion y cursor
-def close_connection_and_cursor(conn, cur):
-    cur.close()
-    conn.close()
-
 def get_admin_by_id(admin_id):
-    conn, cur = get_cursor()
-    cur.execute("SELECT * FROM admins WHERE id = %s", (admin_id,))
-    admin_data = cur.fetchone()
-    close_connection_and_cursor(conn, cur)
+    admin_data = execute_query("SELECT * FROM admins WHERE id = %s", (admin_id,))
 
     if admin_data:
         return Admin(id=admin_data['id'], nickname=admin_data['nickname'], password=admin_data['password'])
+    
     return None
 
 def get_admin_by_nickname(nickname):
-    conn, cur = get_cursor()
-    cur.execute("SELECT * FROM admins WHERE nickname = %s", (nickname,))
-    admin_data = cur.fetchone()
-    close_connection_and_cursor(conn, cur)
+    admin_data = execute_query("SELECT * FROM admins WHERE nickname = %s", (nickname,))
 
     if admin_data:
         return Admin(id=admin_data['id'], nickname=admin_data['nickname'], password=admin_data['password'])
+    
     return None
 
 def create_admin(nickname, password):
     password_hash = generate_password_hash(password)
-
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-
-    # Primero verificamos si el nickname ya existe
-    cur.execute('SELECT * FROM admins WHERE nickname = %s', (nickname,))
-    if cur.fetchone():
+    admin_data = execute_query('SELECT * FROM admins WHERE nickname = %s', (nickname,))
+    
+    if admin_data:
         return None, "Lo sentimos, ese nickname ya está cogido :("
     
     try:
-        cur.execute('INSERT INTO admins (nickname, password) VALUES (%s, %s) RETURNING id, nickname, password', 
-                    (nickname, password_hash))
-        admin_data = cur.fetchone()
-        conn.commit()
+        admin_data = execute_query('INSERT INTO admins (nickname, password) VALUES (%s, %s) RETURNING id, nickname, password', 
+                                   (nickname, password_hash))
     except Exception as e:
-        conn.rollback()
         return None, str(e)
-
-    cur.close()
-    conn.close()
 
     return Admin(admin_data['id'], admin_data['nickname'], admin_data['password']), None
 
 def update_password(admin_id, new_password):
-    conn = get_connection()
-    cur = conn.cursor()
-
     hashed_password = generate_password_hash(new_password)
 
     try:
-        cur.execute('UPDATE admins SET password = %s WHERE id = %s', (hashed_password, admin_id))
-        conn.commit()
+        execute_query('UPDATE admins SET password = %s WHERE id = %s', (hashed_password, admin_id))
+
     except Exception as e:
-        conn.rollback()
         print("Error al actualizar la contraseña: ", str(e))
         return False
-    finally:
-        cur.close()
-        conn.close()
-
+    
     return True
 
 def delete_admin(admin_id):
-    conn, cur = get_cursor()
-
     try:
-        cur.execute("DELETE FROM admins WHERE id = %s", (admin_id,))
-        conn.commit()
+        execute_query("DELETE FROM admins WHERE id = %s", (admin_id,))
+        
     except Exception as e:
-        conn.rollback()
         print(f"Error deleting admin: {str(e)}")
         return None
-    finally:
-        close_connection_and_cursor(conn, cur)
--- src/connection.py
+++ src/connection.py
@@ -1,7 +1,7 @@
 # connection.py
 from os import environ
 from dotenv import load_dotenv
-from psycopg2 import connect
+from psycopg2 import connect, extras
 
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
@@ -17,3 +17,10 @@ password = environ.get('DB_PASSWORD')
 def get_connection():
     conn = connect(host=host, port=port, dbname=dbname, user=username, password=password)
     return conn
+
+def execute_query(query, params):
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
+            cur.execute(query, params)
+            if cur.description:
+                return cur.fetchone()
